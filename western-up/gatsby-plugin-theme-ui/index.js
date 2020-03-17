ec5d4cba0fc8bdbad7828b46933e9b833f5090b0
Evan Willhite <evan@fourkitchens.com>
2020-03-17 11:44:21 -0500

    removed western u refs

--- western-up/gatsby-plugin-theme-ui/index.js
+++
@@ -1,9 +0,0 @@
-import emulsifyTheme from "gatsby-theme-emulsify/src/gatsby-plugin-theme-ui";
-import merge from "lodash.merge";
-
-export default merge({}, emulsifyTheme, {
-  // Custom items here
-  // colors: {
-  //   text: "#6f6f6f",
-  // },
-})
