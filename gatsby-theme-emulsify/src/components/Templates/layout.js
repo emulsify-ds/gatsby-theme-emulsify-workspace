1461e6aea46cbc29a7c76d7b9adc3a7841b364a8
Evan Willhite <evan@fourkitchens.com>
2020-03-18 11:36:11 -0500

    remove headings and toc:

--- gatsby-theme-emulsify/src/components/Templates/layout.js
+++ gatsby-theme-emulsify/src/components/Templates/layout.js
@@ -13,34 +13,6 @@ import TableOfContents from "../Molecules/TableOfContents/TableOfContents.compon
 import StorybookComponent from "../Atoms/StorybookComponent/StorybookComponent.component";
 import DarkWrapper from "../Atoms/DarkWrapper/DarkWrapper.component";
 
-const HEADINGS = [
-  ({ children, props }) => <h1 {...props}>{children}</h1>,
-  ({ children, props }) => <h2 {...props}>{children}</h2>,
-  ({ children, props }) => <h3 {...props}>{children}</h3>,
-  ({ children, props }) => <h4 {...props}>{children}</h4>,
-  ({ children, props }) => <h5 {...props}>{children}</h5>,
-  ({ children, props }) => <h6 {...props}>{children}</h6>
-];
-
-const Heading = level => ({ props, children }) => {
-  const H = HEADINGS[level - 1];
-  const name =
-    typeof children === "string"
-      ? children.replace(/\s+/g, "-").toLowerCase()
-      : "";
-  return (
-    <a
-      name={name}
-      href={`#${name}`}
-      sx={{
-        color: "text"
-      }}
-    >
-      <H {...props}>{children}</H>
-    </a>
-  );
-};
-
 export default props => {
   const { pageContext } = props;
   const post = props.data.mdx;
@@ -50,12 +22,6 @@ export default props => {
    * so that they can use MDX to layout their component documentation, usage, etc.
    */
   const [components] = React.useState({
-    h1: Heading(1),
-    h2: Heading(2),
-    h3: Heading(3),
-    h4: Heading(4),
-    h5: Heading(5),
-    h6: Heading(6),
     TableOfContents: () => (
       <TableOfContents items={post.tableOfContents.items} />
     ),
