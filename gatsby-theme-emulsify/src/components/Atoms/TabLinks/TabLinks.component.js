df121915231310df9805797ae9953c9b911f938b
Evan Willhite <evan@fourkitchens.com>
2020-03-23 19:04:27 -0500

    styling WIP

--- gatsby-theme-emulsify/src/components/Atoms/TabLinks/TabLinks.component.js
+++ gatsby-theme-emulsify/src/components/Atoms/TabLinks/TabLinks.component.js
@@ -6,18 +6,55 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import "./tablinks.css";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
+import ExternalIcon from "./ExternalIcon.js";
 
 /**
  * Component that renders a Tab link interface.
  */
 const Tabs = ({ children, defaultTab }) => {
   return (
-    <nav>
-      <ol>
-        <li class="default">{defaultTab}</li>
+    <nav
+      sx={{
+        borderBottom: theme => `2px solid ${theme.colors.grayDark}`
+      }}
+    >
+      <ol
+        sx={{
+          backgroundColor: "muted",
+          color: "text",
+          margin: 0,
+          listStyle: "none",
+          fontSize: 0
+        }}
+      >
+        <li
+          class="default"
+          sx={{
+            backgroundColor: "accent",
+            color: "background",
+            display: "inline-block",
+            margin: 0,
+            padding: 3,
+            pl: 3
+          }}
+        >
+          {defaultTab}
+        </li>
         {React.Children.map(children, link => (
-          <li>{link}</li>
+          <li
+            sx={{
+              display: "inline-block",
+              margin: 0,
+              padding: 3,
+              pl: 3
+            }}
+          >
+            {link}
+            <ExternalIcon />
+          </li>
         ))}
       </ol>
     </nav>
