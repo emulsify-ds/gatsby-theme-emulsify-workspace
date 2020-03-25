522c87098a040f62e906abb2979094674e1b747f
Evan Willhite <evan@fourkitchens.com>
2020-03-25 11:38:16 -0500

    better docs, readme

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  pathPrefix: "/gatsby-theme-emulsify-workspace",
+  pathPrefix: '/gatsby-theme-emulsify-workspace', // for deploying to a folder like on Github pages.
   plugins: [
     {
       resolve: 'gatsby-theme-emulsify',
       options: {
-        componentLibPath: 'components', // Where your component library lives
-        docPagesPath: 'styleguide', // Where your custom styleguide pages live
-        UILibPath: "https://lucid-colden-c2abf5.netlify.com/iframe.html", // Where your Storybook instance lives
-        basePath: __dirname, // Needed to make above paths relative to your project
-        // designSystems: [
-        //   {
-        //     name: "Basic", // Other design system you may want to link to in a parent/child situation
-        //     link: "/"
-        //   },
-        // ],
         // Site Metadata for style guide
         siteMetadata: {
           title: 'Example',
           description: '',
           author: '',
         },
+        docPagesPath: 'styleguide', // Where your custom style guide pages live
+        componentLibPath: 'components', // Where your component library lives
+        UILibPath: 'https://lucid-colden-c2abf5.netlify.com/iframe.html', // Where your Storybook instance lives. Could be remote or local like "/storybook/iframe.html"
+        basePath: __dirname, // Needed to make above paths relative to your project
+        // designSystems: [
+        //   {
+        //     name: "Basic", // Other design system you may want to link to in a parent/child situation
+        //     link: "/"
+        //   },
+        // ]
       },
     },
   ],
