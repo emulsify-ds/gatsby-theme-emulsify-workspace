113ed8a3a626e8f0549605df02d8cc2bfa2ab184
Evan Willhite <evan@fourkitchens.com>
2020-06-17 13:39:31 -0500

    switch to theme provider

--- gatsby-theme-emulsify/src/components/Templates/layout.js
+++ gatsby-theme-emulsify/src/components/Templates/layout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { MDXProvider } from "@mdx-js/react";
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { jsx, ThemeProvider } from "theme-ui";
+import theme from "../../gatsby-plugin-theme-ui/index";
 
 import Site from "./Site";
 import SEO from "./seo";
@@ -37,7 +37,7 @@ export default (props) => {
   const docPages = props.data.allMdx.edges;
   const componentNodes = props.data.allFile.nodes;
   return (
-    <MDXProvider components={components}>
+    <ThemeProvider theme={theme} components={components}>
       <Site
         collection={post.fields.collection}
         id={post.id}
@@ -56,7 +56,7 @@ export default (props) => {
         description={post.frontmatter.description || post.excerpt}
         keywords={[`gatsby`, `application`, `react`]}
       />
-    </MDXProvider>
+    </ThemeProvider>
   );
 };
 
